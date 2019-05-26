4562f3317edd1d955f792f26e7502889dcb3abf4
Davit Margvelashvili <davit.margvelashvili@gmail.com>
2019-05-26 14:26:01 +0400

    Added: AuctioneerView, HistoryView, LotView

---
+++ Auction/Auction.Wpf.UI/Utils/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auction.Wpf.UI.Utils
+{
+    public static class Extensions
+    {
+        public static bool CanAppendWithDigit(this string textNumber, string digit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
